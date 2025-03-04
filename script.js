62d9afd7859192736871056e5298c77b0c82272f
thanakorn-not <thanakorningkawara@gmail.com>
2025-03-04 14:25:33 +0700

    edit

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 function fetchAndDisplayUsers() {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok" + response.statusText);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const userContainer = document.getElementById("users-container");
-      userContainer.innerHTML = "";
-
-      data.forEach(user => {
-        const userDiv = document.createElement("div");
-        userDiv.innerHTML = `
-            <h2>${user.name}</h2>
-            <p><strong>Email :</strong>${user.email}</p>
-            <p><strong>Phone :</strong>${user.phone}</p>
-            <p><strong>Website :</strong>${user.website}</p>
-            <p><strong>Address :</strong>${user.address.city},<strong>Street:</strong>${user.address.street},<strong>Suite:</strong>${user.address.suite},<strong>Zipcode:</strong>${user.address.zipcode}</p>
-        `;
-
-        userContainer.appendChild(userDiv);
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok" + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        const userContainer = document.getElementById("users-container");
+        userContainer.innerHTML = "";
+  
+        data.forEach(user => {
+          const userDiv = document.createElement("div");
+          userDiv.classList.add("user-card"); 
+  
+          userDiv.innerHTML = `
+              <h2>${user.name}</h2>
+              <p><strong>Email:</strong> ${user.email}</p>
+              <p><strong>Phone:</strong> ${user.phone}</p>
+              <p><strong>Website:</strong> ${user.website}</p>
+              <p><strong>Address:</strong> ${user.address.city}, 
+                 <strong>Street:</strong> ${user.address.street}, 
+                 <strong>Suite:</strong> ${user.address.suite}, 
+                 <strong>Zipcode:</strong> ${user.address.zipcode}
+              </p>
+          `;
+  
+          userContainer.appendChild(userDiv);
+        });
+      })
+      .catch((error) => {
+        console.error("There was a problem with the fetch operation", error);
       });
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation", error);
-    });
-}
-
-fetchAndDisplayUsers();
+  }
+  
+  fetchAndDisplayUsers();
+  
